dd0e9cc2d49d85b5617ec9f341f4f8664ec654b7
nstung7 <nsontung.dev@gmail.com>
2022-05-11 09:26:33 +0700

    Created Projects

---
+++ BirchClustering/Basic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BirchClustering/Testing/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MathNet.Numerics.Distributions;
+
+namespace Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new FisherSnedecor(4, 2);
+            var p2 = f.CumulativeDistribution(19.84);
+            var p = f.DensityLn(19.84);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
